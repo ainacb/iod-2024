b1fffd28aaecff58827549b69b4020df46cc84a6
AinaCB <ainacbautista@gmail.com>
2024-02-24 10:38:24 +1100

    WIP

--- Module1/backend/helloworld.js
+++ Module1/backend/helloworld.js
@@ -1,1 +1,12 @@
-console.log("hello world")
+// console.log("hello world")
+
+// Functions
+
+// Requires:
+// 1. Keyword function
+// 2. Name of the function
+// 3. Parentheses - pass parameters into the function
+// 4. Curly braces - body of the function
+
+function sayHello() {
+    console.log("Hello World!");
