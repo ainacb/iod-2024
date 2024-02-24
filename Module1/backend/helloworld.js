e710a7d46a27e415bde2028caaca65e08b72fb31
AinaCB <ainacbautista@gmail.com>
2024-02-24 16:49:42 +1100

    WIP

--- Module1/backend/helloworld.js
+++ Module1/backend/helloworld.js
@@ -8,5 +8,167 @@
 // 3. Parentheses - pass parameters into the function
 // 4. Curly braces - body of the function
 
-function sayHello() {
-    console.log("Hello World!");
+// function sayHello(firstName) {
+//    console.log("Hello World!");
+//    console.log(`Hello ${firstName}`); //template literals -requires backticks }
+
+// Call the function
+// When you call a function, you use the function name followed by parenthesis
+
+// sayHello("Aina");
+
+//function sum(a, b){
+//    console.log(a + b); 
+// }
+
+// sum(14, 21);
+
+//function calculateTotal(quantity, price) {
+//    return quantity * price;
+//}
+
+// console.log(totalPrice(5, 10));
+
+//const totalPrice = calculateTotal(5, 20);
+
+//console.log(totalPrice);
+
+
+function addNumber(a, b){
+//    console.log(a + b);
+}
+
+addNumber(10, 48);
+
+function subtractNumber(a, b){
+//    console.log(a - b);
+}
+
+subtractNumber(50, 15);
+
+function multipleNumber(a, b){
+//    console.log(a * b);
+}
+
+multipleNumber(20, 4)
+
+function divideNumber(a, b){
+//    console.log(a / b);
+}
+
+divideNumber(72, 9);
+
+function sayHello(firstName) {
+//    console.log("Hello"+" "+firstName); //Concatenate strings
+}
+//sayHello("Aina!!!");
+
+
+// const Sentiment = require('sentiment');
+
+// const sentiment = new Sentiment();
+// const result = sentiment.analyze('Cats are intelligent.');
+
+// console.dir(result); // Score: -2, Comparative: -0.666
+
+// console.log('this is a Message')
+// console.info('this is info')
+// console.warn('this is a warning')
+// console.error('this is an error');
+
+
+// function myFunc() {
+//     var name = "Aina"
+// }
+
+// let helloText = "say Hi";
+// let check = 4;
+// if (check > 3) {
+// let hello = "say Hello instead";
+// console.log(hello);// "say Hello instead"
+// }
+// console.log(hello) // hello is not defined
+
+
+
+
+// const names = ["Adam", "Padma", "Hasith", "Aina", "Chinonye"];
+
+// console.log(names[0]);
+// console.log(names[1]);
+// console.log(names[2]);
+// console.log(names[3]);
+// console.log(names[4]);
+
+// names[3] = "Aina";
+
+// console.log(names[3]);
+
+
+
+let volcanoes = [ 'Mount Vesuvius', 'Mount Etna', 'Mount Fuji' ];
+let fujiIndex = volcanoes.indexOf('Mount Fuji');
+// console.log(fujiIndex); // 2 (indexes start at 0
+
+let numVolcanoes = volcanoes.length;
+// console.log(numVolcanoes); // 3
+
+
+//5 element array
+
+// let rankLevel = ["S", "A", "B", "C", "D"];
+
+// console.log(rankLevel[0]);
+// console.log(rankLevel[1]);
+// console.log(rankLevel[2]);
+// console.log(rankLevel[3]);
+// console.log(rankLevel[4]);
+
+// //replace value
+
+// rankLevel[1] = "Diamond";
+// console.log(rankLevel[1]);
+
+// rankLevel[4] = "Bronze";
+// console.log(rankLevel[4]);
+
+// //new element at beginning of array
+
+// rankLevel.unshift("Chrome");
+// console.log(rankLevel);
+
+// //remove element at end
+
+// rankLevel.pop();
+// console.log(rankLevel);
+
+
+
+
+// const user = {
+//     first_name: "Aina",
+//     last_name: "Bautista",
+//     age: "30",
+//     followers: "987"
+// }
+
+// console.log(user.age);
+
+
+// const book = {
+//     title: "Noli Me Tangere",
+//     description: "Touch me not",
+//     author: "Jose Rizal",
+//     number_of_pages: "936",
+// }
+
+// console.log(book.title);
+// console.log(book.description);
+// console.log(book.author);
+// console.log(book.number_of_pages);
+// console.log(book);
+
+// book.description="El Fili"
+// console.log(book.description)
+
+
